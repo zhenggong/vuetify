a8d77447554f78793755d14859c381645fc04e95
Zheng Gong <zheng.gong@gree.net>
2020-02-20 13:46:44 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+<v-navigation-drawer app v-model="drawer" clipped >
+  <v-container>
+    <v-list-item>
+      <v-list-item-content>
+
+        <v-list-item-title class="title grey--text text--darken-2">
+          Navigation lists
+        </v-list-item-title>
+
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+  </v-container>
+</v-navigation-drawer>
+    
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-menu offset-y>
+        <template v-slot:activator="{on}">
+        <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+        </template>
+          <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data(){
+    return{
+        items: [
+          {name: 'Consulting and suppourt',icon: 'mdi-vuetify'},
+          {name: 'Discord community',icon: 'mdi-discord'},
+          {name: 'Report a bug',icon: 'mdi-bug'},
+          {name: 'Github issue board',icon: 'mdi-github-face'},
+          {name: 'Stack overview',icon: 'mdi-stack-overflow'},
+        ],
+        drawer: null,
+        nav_lists:[
+         {name: 'Getting Started',icon: 'mdi-vuetify'},
+         {name: 'Customization',icon: 'mdi-cogs'},
+         {name: 'Styles & animations',icon: 'mdi-palette'},
+         {name: 'UI Components',icon: 'mdi-view-dashboard'},
+         {name: 'Directives',icon: 'mdi-function'},
+         {name: 'Preminum themes',icon: 'mdi-vuetify'},
+        ],
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
