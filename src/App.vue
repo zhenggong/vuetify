5127dae60080439ca47d3d44e80364ba5539b585
Zheng Gong <zheng.gong@gree.net>
2020-03-03 19:28:01 +0900

    add account icon

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,24 @@
       <v-toolbar-title>Vuetify</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
+      
+        <v-menu offset-y>
+        <template v-slot:activator="{on}">
+        <v-btn v-on="on" text><v-icon>mdi-account</v-icon></v-btn>
+        </template>
+          <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
         <v-menu offset-y>
         <template v-slot:activator="{on}">
         <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
@@ -72,6 +90,10 @@ export default {
           {name: 'Report a bug',icon: 'mdi-bug'},
           {name: 'Github issue board',icon: 'mdi-github-face'},
           {name: 'Stack overview',icon: 'mdi-stack-overflow'},
+        ],
+        accountmenuitems: [
+          {name: 'Sign Up',},
+          {name: 'Login'},
         ],
         drawer: null,
         nav_lists:[
