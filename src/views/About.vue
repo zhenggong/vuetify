c3f3b440e78a589f0bdb9d62bce5cb880f760d66
Zheng Gong <zheng.gong@gree.net>
2020-03-03 15:53:31 +0900

    add axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<v-content>
+        <v-container fluid fill-height>
+          <v-flex xs12 sm6 offset-sm3>
+            <v-card>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+                aspect-ratio="2.75"
+              ></v-img>
+
+              <v-card-actions>
+                <!-- ユーザー情報を取得するボタンを設置 -->
+                <v-btn flat outline color="primary" @click="getUserInfo();"
+                  >データを取得</v-btn
+                >
+              </v-card-actions>
+
+              <v-card-title primary-title>
+                <div>
+                  <!-- ユーザー情報をループ -->
+                  <ul>
+                    <li v-for="(userInfo, index) in userInfoList" :key="index">
+                      {{ userInfo.name.last }}
+                    </li>
+                  </ul>
+                </div>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-container>
+      </v-content>
 </template>
+
+<script>
+// axiosをインポート
+import axios from "axios";
+
+export default {
+  name: 'About',
+  data: function() {
+    return {
+      // 空の配列を用意
+      userInfoList: []
+    };
+  },
+  methods: {
+    getUserInfo: function() {
+      axios
+        .get("https://randomuser.me/api/?results=5")
+        .then(res => {
+          // 成功時
+          console.log(res.data);
+          // userInfoListにapiで取得したユーザー情報を格納
+          this.userInfoList = res.data.results;
+        })
+        .catch(err => {
+          // 失敗時
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
