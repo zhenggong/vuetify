2bc9cac29b485a56ad37ddec5073ca9057f3f1dd
Zheng Gong <zheng.gong@gree.net>
2020-03-04 15:09:41 +0900

    change position

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,31 @@
 <template>
+  <v-container>
+      <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+    </v-row>
+
   <v-form
     ref="form"
     v-model="valid"
@@ -35,8 +62,8 @@
     >
       クリア
     </v-btn>
-
   </v-form>
+      </v-container>
 </template>
 
 <script>
