b110ceedbb5afaff53f364dbd687c54402bcd60e
Zheng Gong <zheng.gong@gree.net>
2020-03-06 12:00:17 +0900

    add micropost content list

--- src/views/Micropost.vue
+++ src/views/Micropost.vue
@@ -4,6 +4,9 @@
       <v-dialog v-model="dialog" persistent max-width="600px">
         <template v-slot:activator="{ on }">
           <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+          <v-btn flat outline color="primary" @click="getUserInfo();"
+                  >データを取得</v-btn
+                >
         </template>
         <v-card>
         <v-container>
@@ -49,31 +52,50 @@
       </v-dialog>
     </v-row>
         <v-container fluid fill-height>
-          <v-flex xs12 sm6 offset-sm3>
-            <v-card>
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-                aspect-ratio="2.75"
-              ></v-img>
-
-              <v-card-actions>
-                <!-- 情報を取得するボタンを設置 -->
-                <v-btn flat outline color="primary" @click="getUserInfo()"
-                  >micropostを取得</v-btn>
-            
-              </v-card-actions>
-
-              <v-card-title primary-title>
-                <div>
-                  <!-- 情報をループ -->
-                  <ul>
-                    <li v-for="(userInfo, index) in userInfoList" :key="index">
-                      {{ userInfo.content}}
-                    </li>
-                  </ul>
-                </div>
-              </v-card-title>
-            </v-card>
+          <v-flex v-for="(micropost, index) in micropostList" :key="index">
+             <v-card
+      max-width="344"
+      class="mx-auto"
+    >
+      <v-list-item>
+        <v-list-item-avatar color="grey"></v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="headline">Micropost</v-list-item-title>
+          <v-list-item-subtitle v-text="micropost.user_id">by Kurt Wagner</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+  
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+        height="194"
+      ></v-img>
+  
+      <v-card-text v-text = micropost.content>
+        Visit ten places on our planet that are undergoing the biggest changes today.
+      </v-card-text>
+  
+      <v-card-actions>
+        <v-btn
+          text
+          color="deep-purple accent-4"
+        >
+          Read
+        </v-btn>
+        <v-btn
+          text
+          color="deep-purple accent-4"
+        >
+          Bookmark
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
           </v-flex>
         </v-container>
       </v-content>
@@ -88,7 +110,7 @@ export default {
   data: function() {
     return {
       // 空の配列を用意
-      userInfoList  : [],
+      micropostList  : [],
       dialog: false,
       content: this.micropost,
           absolute: false,
@@ -115,7 +137,7 @@ export default {
           // 成功時
           console.log(res.data);
           // userInfoListにapiで取得した情報を格納
-          this.userInfoList = res.data.data;
+          this.micropostList = res.data.data;
         })
         .catch(err => {
           // 失敗時
