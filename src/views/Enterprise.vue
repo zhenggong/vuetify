8347cd27a7a663b0f85c1c05f0c2a35c0a77ae13
Zheng Gong <zheng.gong@gree.net>
2020-03-13 13:17:16 +0900

    add about4

--- src/views/Enterprise.vue
+++ src/views/Enterprise.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-tabs class="tabs" centered grow height="60px" v-model="activeTab">
+    <v-tabs class="tabs" height="46px" v-model="activeTab">
       <v-tab v-for="tab in tabs" :key="tab.id" :to="tab.route" exact>{{ tab.name }}</v-tab>
     </v-tabs>
     <router-view></router-view>
@@ -15,7 +15,9 @@ export default {
       activeTab: '/about2',
       tabs: [
         { id: 1, name: "About2", route: '/about2' },
-        { id: 2, name: "About3", route: '/about3'}
+        { id: 2, name: "About3", route: '/about3'},
+        { id: 3, name: "About4", route: '/about2' },
+        { id: 4, name: "About5", route: '/about3'},
       ]
     };
   }
